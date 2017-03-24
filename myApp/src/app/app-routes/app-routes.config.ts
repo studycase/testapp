19ea4809ed84e47e2971173ca3d7090321a1ea7e
karthi <karthihistory@gmail.com>
2017-03-24 10:19:24 +0000

    folder structure naming

--- myApp/src/app/app-routes/app-routes.config.ts
+++ myApp/src/app/app-routes/app-routes.config.ts
@@ -1,17 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from '../app.component';
-import { EstimatedEntriesComponent } from '../estimated-entries-page/estimated-entries.component';
-import { SyllabusDetailsComponent } from '../syllabus-details-page/syllabus-details.component';
-import { TestoComponent } from '../testo/testo.component';
-
+import { EstimatedEntriesComponent } from '../estimated-entries/estimated-entries.component';
+import { SyllabusDetailsComponent } from '../syllabus-details/syllabus-details.component';
 
 export const routes: Routes = [
     //{ path: '',   redirectTo: '',  pathMatch: 'full'  },
     //{ path : 'home', component : AppComponent},
     { path : 'estimatedEntries', 
         children:[
-                { path : '', component : EstimatedEntriesComponent }, 
-                { path : 'childDetail', component : TestoComponent }
+                { path : '', component : EstimatedEntriesComponent }
             ]
     },
     { path : 'syllabusDetails', component : SyllabusDetailsComponent }];
--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -16,21 +16,20 @@ import { routes } from './app-routes/app-routes.config';
 
 /* Import component dependencies */
 import { AppComponent } from './app.component';
-import { EstimatedEntriesComponent } from './estimated-entries-page/estimated-entries.component';
-import { SyllabusDetailsComponent } from './syllabus-details-page/syllabus-details.component';
+import { EstimatedEntriesComponent } from './estimated-entries/estimated-entries.component';
+import { SyllabusDetailsComponent } from './syllabus-details/syllabus-details.component';
 
 /* Import service dependencies */
-import { EstimatedEntriesService } from './estimated-entries-page/estimated-entries.service';
-import { SyllabusDetailsService } from './syllabus-details-page/syllabus-details.service';
-import { TestoComponent } from './testo/testo.component';
+import { EstimatedEntriesService } from './estimated-entries/estimated-entries.service';
+import { SyllabusDetailsService } from './syllabus-details/syllabus-details.service';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EstimatedEntriesComponent,
-    SyllabusDetailsComponent,
-    TestoComponent
+    SyllabusDetailsComponent
   ],
   imports: [
     BrowserModule,
